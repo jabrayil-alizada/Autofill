c14605ad92579d7e18ab3586b9a0e831915c19a8
Jabrayil <cabi10@mail.ru>
2016-08-02 18:34:37 +0400

    Started working

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Autofill by Jabrayil Alizada</title>
+	<style>
+		.parent {
+			width: 600px;
+			margin:0 auto;
+		}
+
+		.parent:hover {
+			cursor: pointer;
+		}
+	</style>
+</head>
+<body>
+
+<div class="parent"> Test &darr;</div>
+
+</body>
+</html>
