25a10920b7b20588dda081dca9ac5bd25f902c68
Jabrayil <cabi10@mail.ru>
2016-08-02 19:02:18 +0400

    Scratch

--- index.php
+++ index.php
@@ -2,20 +2,27 @@
 <html>
 <head>
 	<title>Autofill by Jabrayil Alizada</title>
-	<style>
-		.parent {
-			width: 600px;
-			margin:0 auto;
-		}
-
-		.parent:hover {
-			cursor: pointer;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<meta charset="utf-8">
 </head>
 <body>
+<div class="main">
 
-<div class="parent"> Test &darr;</div>
+	<div class="parent"> Test &darr;</div>
+	<div class="clr"></div>
+	<div id="popup" class="popup">
+		<p> Выберите свой населенный пункт</p>
+		<ul>
+			<li><a id="baku" href="#">Баку</a></li>
+			<li><a id="ganja" href="#">Гянджа</a></li>
+			<li><a id="shirvan" href="#">Ширван</a></li>
+		</ul>
+		<p> Или выберите другой</p>
+		<form>
+			<input type="text" name="search">
+		</form>
+	</div>
 
+</div>
 </body>
 </html>
